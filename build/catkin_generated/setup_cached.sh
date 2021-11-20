d182f17c088bb7d61f0218a129e878e8c976ed54
Heyuan5757 <yuanhe@deeproute.ai>
2021-11-20 16:17:12 +0800

    catkin

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yuanhe/Practice/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/yuanhe/Practice/devel/lib:/opt/ros/melodic/lib:/opt/deeproute/planning/lib:/opt/deeproute/ctrl/lib:/opt/deeproute/planning_sim/lib:/usr/local/cuda/lib64:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins/:/usr/local/lib/:/usr/local/cuda/lib64'
+export PWD='/home/yuanhe/Practice/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/yuanhe/Practice/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/yuanhe/Practice/src:$ROS_PACKAGE_PATH"
